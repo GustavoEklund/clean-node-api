47aa5991d5a59a7102dfe3378948616298a6f9ba
Gustavo Eklund <gustavo04616@gmail.com>
2021-01-02 14:18:03 -0300

    move LoginRouter to ist own file

---
+++ src/presentation/routers/login-router.js
@@ -0,0 +1,16 @@
+const HttpResponse = require('../helpers/http-response')
+
+module.exports = class LoginRouter {
+  execute (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
+    }
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}
